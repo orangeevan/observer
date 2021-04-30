927f6b3b7b06bfb726639769bc3a232e90c07d53
yuen <494926093@qq.com>
2021-04-30 09:59:01 +0800

    observer

---
+++ src/main/java/com/yuen/Start.java
@@ -0,0 +1,31 @@
+package com.yuen;
+
+import com.yuen.observer.ObserverController;
+
+/**
+ * @author: yuen.cy
+ * @description:
+ * @since 9:45 2021/4/30
+ */
+public class Start {
+    public static void main(String[] args) {
+        ObserverController controller = new ObserverController();
+        new Thread(() -> {
+            controller.attachForEver(Start.Event.class, () -> System.out.println("1"));
+        }).start();
+        new Thread(() -> {
+            controller.attachOne(Start.Event.class, () -> System.out.println("2"));
+        }).start();
+        new Thread(() -> {
+            controller.fire(Start.Event.class);
+        }).start();
+        new Thread(() -> {
+            controller.fire(Start.Event.class);
+        }).start();
+
+    }
+
+    public static interface Event {
+    }
+}
+
